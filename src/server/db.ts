2630afdf74e86402dd55e993b7701248da40078f
Patryk <patryk.studniak@gmail.com>
2023-02-24 12:09:30 +0100

    test commit

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
