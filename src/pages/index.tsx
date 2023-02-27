367afdd0be4a2de535b6d23a077bc56c71d3c060
Patryk <patryk.studniak@gmail.com>
2023-02-27 12:00:21 +0100

    added tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { Header } from "~/components";
+import { api } from "~/utils/api";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const { data: sessionData } = useSession();
+  const { data: groups, refetch: refetchGroups } =
+    api.expenseGroup.getAllGroups.useQuery(undefined, {
+      enabled: !!sessionData?.user,
+    });
+  const createGroup = api.expenseGroup.createGroup.useMutation({
+    onSuccess: () => {
+      void refetchGroups();
+    },
+  });
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <>
       <Head>
@@ -15,6 +30,15 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <Header />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={() => void createGroup.mutate({ title: inputValue })}>
+          Create group
+        </button>
+        <pre>{JSON.stringify(groups)}</pre>
       </main>
     </>
   );
